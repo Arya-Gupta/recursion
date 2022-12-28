6ff8d61f934d2fdb3e18b97037cdd8d8b67fc974
unknown <arya.gupta99999@gmail.com>
2022-12-28 16:24:24 +0530

    kth symbol in grammar

---
+++ kth_symbol_in_grammar.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthGrammar(int n, int k) {
+    if(n == 1 && k == 1) return 0;
+    int mid = pow(2, n - 1) / 2;
+    if(k <= mid) return kthGrammar(n - 1, k);
+    else return !kthGrammar(n - 1, k - mid);
+}
+
+int main(){
+    int n, k;
+    cout << "n = ";
+    cin >> n;
+    cout << "k = ";
+    cin >> k;
+    cout << kthGrammar(n, k);
+}
