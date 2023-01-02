4c14d37d82a4ec33d6ba5d84aeb4fb320100e145
unknown <arya.gupta99999@gmail.com>
2023-01-03 01:07:52 +0530

    permutation with mixed cases

---
+++ permutation_mixed_cases.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void findPermutations(string ip, string op, vector<string> &permutations)
+{
+    if (ip == "")
+    {
+        permutations.push_back(op);
+        return;
+    }
+    string op1 = op, op2 = op;
+    char ch = ip.front();
+    op1.push_back(ch);
+    op2.push_back(toupper(ch));
+    ip.erase(ip.begin());
+    findPermutations(ip, op1, permutations);
+    findPermutations(ip, op2, permutations);
+}
+
+int main()
+{
+    string s;
+    cout << "Enter the string = ";
+    cin >> s;
+    vector<string> permutations;
+    findPermutations(s, "", permutations);
+    for (auto i : permutations)
+        cout << i << endl;
+}
