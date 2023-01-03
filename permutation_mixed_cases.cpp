0196e430fe5ddbff43196db63e9261b5f5aa8bf2
unknown <arya.gupta99999@gmail.com>
2023-01-03 18:12:12 +0530

    updated

--- permutation_mixed_cases.cpp
+++ permutation_mixed_cases.cpp
@@ -8,13 +8,26 @@ void findPermutations(string ip, string op, vector<string> &permutations)
         permutations.push_back(op);
         return;
     }
-    string op1 = op, op2 = op;
+
     char ch = ip.front();
-    op1.push_back(ch);
-    op2.push_back(toupper(ch));
-    ip.erase(ip.begin());
-    findPermutations(ip, op1, permutations);
-    findPermutations(ip, op2, permutations);
+
+    if (isalpha(ch))
+    {
+        string op1 = op, op2 = op;
+        op1.push_back(toupper(ch));
+        op2.push_back(tolower(ch));
+        ip.erase(ip.begin());
+        findPermutations(ip, op1, permutations);
+        findPermutations(ip, op2, permutations);
+    }
+
+    else if (isdigit(ch))
+    {
+        string op1 = op;
+        op1.push_back(ch);
+        ip.erase(ip.begin());
+        findPermutations(ip, op1, permutations);
+    }
 }
 
 int main()
